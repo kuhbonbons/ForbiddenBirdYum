8dca14150c47c76287e0c17c6a8b3633bd057005
theforbiddenpool <contact@theforbiddenpool.com>
2020-10-07 20:48:19 +0100

    feat(editor): textarea shrinks/grows according to input text height

--- components/NoteEditor/Editor/Heading.js
+++ components/NoteEditor/Editor/Heading.js
@@ -1,14 +1,28 @@
+import { useState, useRef, useEffect } from 'react';
 import styles from './Editor.module.scss';
 
 export default function Heading({
   name, placeholder, value, setValue,
 }) {
+  const el = useRef();
+  const [currentHeight, setCurrentHeight] = useState();
+
   const removeNewLine = (text) => text.replace(/\n/g, '');
 
   const handleChange = (e) => {
     const text = removeNewLine(e.currentTarget.value);
+
+    setCurrentHeight(0);
     setValue(text);
   };
+
+  useEffect(() => {
+    setCurrentHeight(el.current.scrollHeight);
+  }, []);
+
+  useEffect(() => {
+    if (currentHeight === 0) { setCurrentHeight(el.current.scrollHeight); }
+  }, [currentHeight]);
 
   return (
     <>
@@ -18,7 +32,17 @@ export default function Heading({
         onChange={handleChange}
         placeholder={placeholder}
         value={value}
+        maxLength="255"
+        rows="1"
+        ref={el}
       />
+      <style jsx>
+        {`
+        textarea[name=${name}] {
+          height: ${currentHeight}px;
+        }
+      `}
+      </style>
     </>
   );
 }
