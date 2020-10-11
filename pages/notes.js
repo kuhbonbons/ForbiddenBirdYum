61d34a146317da98fc0c803ea979cd3f9820c2bf
theforbiddenpool <contact@theforbiddenpool.com>
2020-10-12 00:17:43 +0100

    fix(notes): add viewport meta rule

--- pages/notes.js
+++ pages/notes.js
@@ -1,9 +1,15 @@
-import { NoteEditor } from '../components/'
+import Head from 'next/head';
+import { NoteEditor } from '../components';
 
 export default function Notes() {
   return (
     <div>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>All Notes</title>
+      </Head>
       <NoteEditor />
     </div>
-  )
+  );
 }
